e343ac472beea131268c9760d14d00a18f767df0
Preetham H.M <tysonpreetham@gmail.com>
2025-02-03 22:54:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -130,7 +130,7 @@ async def extract_clips(files:FileNames,model:Model,prompt:str=None):
         # Make the LLM request.
         print("Making LLM inference request...")
         response = model.generate_content(contents,
-                                        request_options={"timeout": 600}, response_mime_type="application/json", response_schema=list[Highlights])
+                                        request_options={"timeout": 600}, generation_config=genai.GenerationConfig(response_mime_type="application/json", response_schema=list[Highlights]))
         print(response.text)   
 
         return JSONResponse(content={"message":f"{response.text}"},status_code=200)
